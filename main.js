3e18ecc5175e45a217b7007de6fbfc408c9f0343
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2025-06-30 07:33:10 -0300

    .

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const pipe = document.querySelector(".pipe")
 const nuvem = document.querySelector(".nuvem")
 const startButton = document.querySelector(".start")
 const gameOverScreen = document.querySelector(".game-over")
+const scoreElement = document.querySelector(".score")
 
 audioStart = new Audio("./sound/audio_theme.mp3");
 const gameOverSound = new Audio("./sound/audio_gameover.mp3");
 
 let gameStarted = false;
+let score = 0; 
 
 const startGame = () => {
     gameStarted = true;
@@ -30,6 +32,14 @@ const jump = () => {
     }
     ,500);
 }
+
+const updateScore = () => {
+    score +=1;
+    scoreElement.textContent = score;
+    const animationSpeed = 1.5/ (1 + score/500);
+    pipe.style.animation = pipe-animation ${animationSpeed}s Infinite linear;
+
+} 
 
 const loop = setInterval(() => {
 const pipePosition = pipe.offsetLeft;
